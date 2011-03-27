ccf6c33e1914a5ec8a4baaab40fca84fa8b98467
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-27 16:42:57 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Muistix::Application.config.secret_token = '73a982e20ffd5a71531ef6f1ee390424bec573f6fd318d65087b4ff17b22dc733f1fb3a15f1d227a2772e4e19b6d31ead69d3e30995bfda19a4a034e1a797b57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Muistix::Application.config.session_store :cookie_store, :key => '_muistix_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Muistix::Application.config.session_store :active_record_store
