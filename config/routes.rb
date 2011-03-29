4b2bd529c717ef1d4e11d6a2c6d6913a24ec3619
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-29 22:00:55 +0300

    Reroute site root to plans index

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Muistix::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :to => "plans#index"
 
   # See how all your routes lay out with "rake routes"
 
