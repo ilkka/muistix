5c54e0dd61953c31175e4606c45db65229e526a6
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-03-29 12:58:46 +0300

    Implement login step

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,11 +1,16 @@
 # Step definitions for logging in
-#
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
 Given /^a User with email "([^"]*)" with password "([^"]*)"$/ do |email, pass|
   User.make(:email => email, :password => pass)
 end
 
-When /^I log in as "([^"]*)"\/"([^"]*)"$/ do |user, pass|
-  pending # express the regexp above with the code you wish you had
+When /^I log in as "([^"]*)"\/"([^"]*)"$/ do |email, pass|
+  fill_in("email", :with => email)
+  fill_in("password", :with => pass)
+  click_button("login")
 end
 
 Then /^I should be logged in$/ do
