0faab53ccab02ae4d9137fcc949866ccb7d807ff
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2011-03-29 12:09:31 +0300

    Fix User blueprint to use email

--- features/support/blueprints.rb
+++ features/support/blueprints.rb
@@ -2,6 +2,6 @@
 require 'machinist/active_record'
 
 User.blueprint do
-  username { Faker::Lorem.words(1).first.downcase }
+  email { Faker::Lorem.words(1).first.downcase }
   password { Faker::Lorem.words(1).first.downcase }
 end
