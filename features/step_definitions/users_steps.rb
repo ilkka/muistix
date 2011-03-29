8b33f82c198ce418fc43ef528fea5c081cdc5727
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-29 20:14:45 +0300

    Fill in password confirmation too

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -34,6 +34,7 @@ When /^I register$/ do
   click_link "register"
   fill_in "user_email", :with => user.email
   fill_in "user_password", :with => user.password
+  fill_in "user_password_confirmation", :with => user.password
   click_button "Sign up"
 end
 
