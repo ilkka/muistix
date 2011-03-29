4e7247e1684f753220c2490303bf13399dad2962
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-03-29 22:24:32 +0300

    Step definitions for single plan scenario

--- features/step_definitions/plan_steps.rb
+++ features/step_definitions/plan_steps.rb
@@ -7,3 +7,15 @@ Then /^I should see my plans$/ do
   page.should have_content(@plan1.description)
   page.should have_content(@plan2.description)
 end
+
+When /^I select a plan$/ do
+  click_link @plan1.description
+end
+
+Then /^I should see the steps for that plan$/ do
+  @plan1.steps.each do |step|
+    page.should have_content(step.objective)
+    page.should have_xpath("//a", :href => step.uri)
+  end
+end
+
