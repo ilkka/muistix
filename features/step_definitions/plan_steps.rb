eb3a4463044c4c43803815164b289cda29903311
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-03 13:38:55 +0300

    Implement plan creation and creation result check steps
    Fill in the steps from the plan blueprint and check that they were saved
    correctly.

--- features/step_definitions/plan_steps.rb
+++ features/step_definitions/plan_steps.rb
@@ -22,7 +22,14 @@ end
 When /^I create a new plan$/ do
   @my_new_plan = Plan.make
   click_link 'New Plan'
-  fill_in "plan_description", :with => @my_new_plan.description
+  fill_in "Description", :with => @my_new_plan.description
+  counter = 0
+  @my_new_plan.steps.each do |step|
+    click_link 'Add step'
+    fill_in "Objective #{counter}", :with => step.description
+    fill_in "URI #{counter}", :with => step.uri
+    counter += 1
+  end
   click_button 'Save'
 end
 
@@ -34,3 +41,9 @@ When /^I select the plan$/ do
   click_link @my_new_plan.description
 end
 
+Then /^I should see the steps for the newly\-created plan$/ do
+  @my_new_plan.steps.each do |step|
+    page.should have_content step.description
+    page.should have_xpath "//a", :href => step.uri
+  end
+end
