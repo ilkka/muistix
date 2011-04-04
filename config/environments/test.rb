37cbb5dc9b99f92addf4e3f8cfcb99b503de0edb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-04 19:39:51 +0300

    Revert "Don't cache classes in test env so that spork works"
    This reverts commit 11f308b7d5fcf1a93ef64aaf69c14c1f78664fcb.
    
    This maybe is messing with Cucumber runs now complaining about "no
    master blueprints".

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,9 +5,7 @@ Muistix::Application.configure do
   # test suite.  You never need to work with it otherwise.  Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
-  #
-  # That having been said, don't cache classes so that spork works.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
